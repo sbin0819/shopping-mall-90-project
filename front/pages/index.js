a5bd7c7b70141423dbe50caadf1da45785effbef
sbinha0819 <sbinha123@gmail.com>
2020-08-24 00:14:36 +0900

    add index.js

---
+++ front/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Wellcome!</h1>
+            <h2>Shopping mall Project 90</h2>
+        </>
+    )
+}
+
+export default Home;
