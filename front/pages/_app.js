9e3deb3498b16abfb952cc4f7503b786eff69fa5
sbinha0819 <sbinha123@gmail.com>
2020-08-31 07:36:53 +0900

    add redux

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import HEAD from 'next/head'
+import HEAD from 'next/head';
 import 'antd/dist/antd.css';
 
-
 const App = ({ Component }) => {
-    return (
-        <>
-            <HEAD>
-                <meta charSet='utf-8' />
-                <title>NodeBird</title>
-            </HEAD>
-            <Component />
-        </>
-    )
-}
+  return (
+    <>
+      <HEAD>
+        <meta charSet='utf-8' />
+        <title>쇼핑 90</title>
+      </HEAD>
+      <Component />
+    </>
+  );
+};
 
-
-
-export default App
+export default App;
